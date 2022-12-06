d00d9e433e55d6a6dc9da65e9400af94b9b30e25
kapadokia <ktnyandoch@gmail.com>
2022-12-07 02:04:55 +0300

    Necessary controlllers to handle the view layer

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,26 @@
+class CardsController < ApplicationController
+
+    def index
+        cards = Card.all
+        render json:cards
+    end
+
+    def show
+        card = Card.find_by(id: params[:id])
+        render json:card
+    end
+
+    def create
+        card = Card.create(card_params)
+        if card.valid?
+          session[:card_id] = card.id
+          render json: card, status: :created
+        else
+          render json: { errors: card.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def card_params
+        params.permit(:card_name, :card_number,:user_id, :expiration, :card_bank, :cvv, :amount)
+      end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+
+    def index
+        users=User.all
+        render json:users
+    end
+    def create
+        user = User.create(user_params)
+        
+        if user.valid?
+          session[:user_id] = user.id
+          render json: user, status: :created
+          
+          # initial data
+          card_data = {
+            amount: "1000",
+            card_name:user.username, 
+            card_number:"3458"+user.phone, 
+            user_id:user.id, 
+            expiration:"12/23", 
+            card_bank: "MORICOIN", 
+            cvv:"123"
+          }
+          @card = Card.new(card_data)
+          @card.save
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+    
+      def show
+        user = User.find_by(id: session[:user_id])
+        if user
+          render json: user
+        else
+          render json: { error: "Not authorized" }, status: :unauthorized
+        end
+      end
+    
+      private
+    
+      def user_params
+        params.permit(:username, :password,:email, :gender, :phone, :password_confirmation)
+      end
+     
+end
